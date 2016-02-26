215f70f669d0913e2e2ffb5c2754c0de6189ee84
sungurovm <sungurovm@yahoo.com>
2016-02-25 21:15:57 -0500

    more changes
    more

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@
 
 <body>
 
-    <div class="brand">Business Casual</div>
+    <div class="brand">Contact Information</div>
     <div class="address-bar">07470 34 Parkside Court | Wayne, New Jersey</div>
 
     <!-- Navigation -->
@@ -47,13 +47,12 @@
                     <span class="icon-bar"></span>
                 </button>
                 <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
-                <a class="navbar-brand" href="index.html">Business Casual</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="https://is219s16sungurovp1.herokuapp.com/aboutme.php">About</a>
+                        <a href="https://is219s16sungurovp1.herokuapp.com/">About</a>
                     </li>
                     <li>
                         <a href="https://is219s16sungurovp1.herokuapp.com/services.php">Services</a>
@@ -72,13 +71,12 @@
                 <div class="col-lg-12">
                     <hr>
                     <h2 class="intro-text text-center">Contact
-                        <strong>business casual</strong>
                     </h2>
                     <hr>
                 </div>
                 <div class="col-md-8">
                     <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
-                    <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+                    <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.brandeis.edu/about/images/newformat/map2.jpg"></iframe>
                 </div>
                 <div class="col-md-4">
                     <p>Phone:
